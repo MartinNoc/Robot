132f44d647fe08f4b8c6ae0586734dfef02a13aa
DaniWi <daniel.witsch@student.uibk.ac.at>
2015-04-20 21:31:21 +0200

    Add Camera and Color Blob Detection

--- src/com/example/robotwasd/MainActivity.java
+++ src/com/example/robotwasd/MainActivity.java
@@ -1,23 +1,46 @@
 package com.example.robotwasd;
 
-import com.example.robotwasd.R;
+import java.util.Iterator;
+import java.util.List;
 
 import jp.ksksue.driver.serial.FTDriver;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.OpenCV.ColorBlobDetector;
 
 /**
  * App do controll Robot
  * @author Witsch Daniel, Nocker Martin, Schiborr Mark
  *
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnTouchListener, CvCameraViewListener2 {
 	private Robot robot;
 	private TextView textLog;
 	
@@ -25,9 +48,42 @@ public class MainActivity extends ActionBarActivity {
 	private EditText editTextY;
 	private EditText editTextTheta;
 	
+    private static final String  TAG              = "OCVSample::Activity";
+
+    private boolean              mIsColorSelected = false;
+    private Mat                  mRgba;
+    private Scalar               mBlobColorRgba;
+    private Scalar               mBlobColorHsv;
+    private ColorBlobDetector    mDetector;
+    private Mat                  mSpectrum;
+    private Size                 SPECTRUM_SIZE;
+    private Scalar               CONTOUR_COLOR;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		setContentView(R.layout.activity_main);
 		
 		textLog = (TextView) findViewById(R.id.textView1);
@@ -45,38 +101,38 @@ public class MainActivity extends ActionBarActivity {
 		editTextX.setText("85");
 		editTextY.setText("0");
 		editTextTheta.setText("45");
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView.setCvCameraViewListener(this);
 	}
 	
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+    
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+    
 	@Override
 	protected void onDestroy(){
 		robot.disconnect();
 		super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
 	}
 	
 	
 	
 	/** Button Functions */
-	
-	public void wButton_onClick(View v) {
-		robot.moveForward();
-	}
-	
-	public void xButton_onClick(View v) {
-		robot.moveBackward();
-	}
-	
-	public void sButton_onClick(View v) {
-		robot.stopRobot();
-	}
-	
-	public void aButton_onClick(View v) {
-		robot.turnLeft(90);
-	}
-	
-	public void dButton_onClick(View w) {
-		robot.turnRight(90);
-	}
-	
+		
 	public void minusButton_onClick(View v) {
 		robot.lowerBar();
 	}
@@ -94,23 +150,7 @@ public class MainActivity extends ActionBarActivity {
 	public void upButton_onClick(View v) {
 		robot.upPositionBar();
 	}
-	
-	public void LEDonButton_onClick(View v) {
-		robot.LedOn();
-	}
-	
-	public void LEDoffButton_onClick(View v) {
-		robot.LedOff();
-	}
-	
-	public void readSensorButton_onClick(View v) {
-		textLog.setText(robot.readSensor());
-	}
-	
-	public void SquareButton_onClick(View v) {
-		robot.driveSquare(90.0);
-	}
-	
+		
 	public void connectButton_onClick(View v) {
 		robot.connect();
 	}
@@ -158,4 +198,117 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mDetector = new ColorBlobDetector();
+        mSpectrum = new Mat();
+        mBlobColorRgba = new Scalar(255);
+        mBlobColorHsv = new Scalar(255);
+        SPECTRUM_SIZE = new Size(200, 64);
+        CONTOUR_COLOR = new Scalar(255,0,0,255);
+		
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		mRgba.release();
+		
+	}
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		mRgba = inputFrame.rgba();
+		Mat rotMat = Imgproc.getRotationMatrix2D(new Point(mRgba.cols()/2,mRgba.rows()/2), -90.0, 1);
+		Imgproc.warpAffine(mRgba, mRgba, rotMat, mRgba.size());
+
+        if (mIsColorSelected) {
+            mDetector.process(mRgba);
+            List<MatOfPoint> contours = mDetector.getContours();
+            Point lowest = new Point(0,0);
+            if(contours.size() > 0){
+            	for(MatOfPoint matofpoint : contours){
+		            List<Point> contList = matofpoint.toList();
+		            Iterator<Point> iter = contList.iterator();
+		            while(iter.hasNext()){
+		            	Point x = iter.next();
+		            	if(lowest.x < x.x)
+		            		lowest = x;
+		            }
+            	}
+	           	System.out.println("ROBOT: " + lowest.x);
+            }
+            Core.circle(mRgba, lowest, 20, new Scalar(200.0), -1);
+            Log.e(TAG, "Contours count: " + contours.size());
+            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(mBlobColorRgba);
+
+            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+            mSpectrum.copyTo(spectrumLabel);
+        }
+
+        return mRgba;
+	}
+
+	@Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
+        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
+
+        int x = (int)event.getX() - xOffset;
+        int y = (int)event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE);
+
+        mIsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
+	
+	private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
+    }
 }
